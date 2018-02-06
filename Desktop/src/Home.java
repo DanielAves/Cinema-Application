72c5a23b67361a64c62a9f803423e6faad18cbb2
Dan Aves <sc16da@leeds.ac.uk>
2018-02-06 18:01:11 +0000

    Created two basic scenes for Home and Movies

--- Desktop/src/Home.java
+++ Desktop/src/Home.java
@@ -1,32 +1,64 @@
-/**
- * Created by sc16da on 06/02/18.
- */
-import javafx.application.Application;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
+import javafx.application.*;
 import javafx.scene.control.Button;
-import javafx.scene.layout.HBox;
-import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
 
 public class Home extends Application {
 
-    @Override public void start(Stage primaryStage) {
-        Button okButton = new Button("OK");
-        okButton.setStyle("-fx-font: 22 arial;");
-
-        okButton.setOnAction(event -> System.out.println("OK clicked!"));
-
-        HBox pane = new HBox(10);
-        pane.setAlignment(Pos.CENTER);
-        pane.getChildren().add(okButton);
-
-        Scene scene = new Scene(pane, 250, 85);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Button3");
-        primaryStage.show();
-    }
+    Stage window;
+    Scene scene1, scene2;
 
     public static void main(String[] args) {
-        Application.launch(args);
+        launch(args);
     }
+
+    @Override
+    public void start(Stage primaryStage) {
+        window = primaryStage;
+
+        javafx.geometry.Rectangle2D visualBounds = Screen.getPrimary().getVisualBounds();
+
+
+
+        //Button 1
+        Label label1 = new Label("Film 1"); //Movie content from database
+        Button button1 = new Button("Go to");
+        button1.setOnAction(e -> window.setScene(scene2));
+
+        //Layout 1 - children laid out in vertical column
+        StackPane layout1 = new StackPane();
+        layout1.getChildren().addAll(label1, button1);
+        StackPane root = new StackPane();
+        //scene1 = new Scene(layout1, 200, 200);
+        scene1 = new Scene(layout1,visualBounds.getWidth(),visualBounds.getHeight());
+
+
+        //Button 2
+        Button button2 = new Button("Home");
+        button2.setOnAction(e -> window.setScene(scene1));
+
+        //Layout 2
+        StackPane layout2 = new StackPane();
+        layout2.getChildren().add(button2);
+        scene2 = new Scene(layout2,visualBounds.getWidth(),visualBounds.getHeight());
+
+        //Display scene 1 at first
+        window.setScene(scene1);
+        window.setTitle("Desktop Interface");
+        window.show();
+
+
+
+
+    }
+
+
+
 }
+
