d5459827b33b69ead92ed97719ef883d52ba6224
Daniel Aves <sc16da@leeds.ac.uk>
2018-02-11 23:57:31 +0000

    Layout completed for Scene 2 'Film Times'

--- Desktop/src/Home.java
+++ Desktop/src/Home.java
@@ -1,4 +1,6 @@
 import javafx.application.*;
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
 import javafx.geometry.*;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
@@ -9,9 +11,11 @@ import javafx.scene.layout.*;
 import javafx.scene.control.*;
 
 import java.awt.*;
+
+import java.awt.Window;
 import java.awt.geom.Rectangle2D;
 
-public class Home extends Application {
+public class Home extends Application{
 
     Stage window;
     Scene HomeScreen, FilmTimes;
@@ -21,10 +25,19 @@ public class Home extends Application {
 
     }
 
+
+
+
+
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws Exception{
 
         java.util.Date date = new java.util.Date();
+
+
+
+
+
         //System.out.println(date);
 
         window = primaryStage;
@@ -42,12 +55,14 @@ public class Home extends Application {
         window.setScene(HomeScreen);
         window.show();
 
-        Label Date = new Label();
+
+        Label Date = new Label(String.valueOf(date));
         Label Film1 = new Label("Film 1 Text"); //Movie content from database
         Label Film2 = new Label("Film 2 Text");
         Label Film3 = new Label("Film 3 Text");
         Label Film4 = new Label("Film 4 Text");
-        Label Film5 = new Label("Film 6 Text");
+        Label Film5 = new Label("Film 5 Text");
+        Label TimeFor = new Label("Displaying times for    ");
 
         Button Film1Button = new Button("Go to");
         Film1Button.setPrefHeight(50);
@@ -65,16 +80,30 @@ public class Home extends Application {
         Film5Button.setPrefHeight(50);
         Film5Button.setPrefWidth(100);
 
-        
+
 
         //Links for elements
-        //int i = 0;
 
-        Film1Button.setOnAction(e -> window.setScene(FilmTimes));
-        Film2Button.setOnAction(e -> window.setScene(FilmTimes));
-        Film3Button.setOnAction(e -> window.setScene(FilmTimes));
-        Film4Button.setOnAction(e -> window.setScene(FilmTimes));
-        Film5Button.setOnAction(e -> window.setScene(FilmTimes));
+        Film1Button.setOnAction(e ->{
+            TimeFor.setText(TimeFor.getText() + String.valueOf(Film1));
+            window.setScene(FilmTimes);
+        });
+        Film2Button.setOnAction(e ->{
+            TimeFor.setText(TimeFor.getText() + String.valueOf(Film2));
+            window.setScene(FilmTimes);
+        });
+        Film3Button.setOnAction(e ->{
+            TimeFor.setText(TimeFor.getText() + String.valueOf(Film3));
+            window.setScene(FilmTimes);
+        });
+        Film4Button.setOnAction(e ->{
+            TimeFor.setText(TimeFor.getText() + String.valueOf(Film4));
+            window.setScene(FilmTimes);
+        });
+        Film5Button.setOnAction(e ->{
+            TimeFor.setText(TimeFor.getText() + String.valueOf(Film5));
+            window.setScene(FilmTimes);
+        });
 
 
 
@@ -90,37 +119,95 @@ public class Home extends Application {
         GridPane.setConstraints(Film4, 5,40);
         GridPane.setConstraints(Film5Button, 120,50);
         GridPane.setConstraints(Film5, 5,50);
+        GridPane.setConstraints(Date,150,0);
 
-        Date.setText(String.valueOf(date));
 
 
         //Add all contents to Grid
         grid.getChildren().addAll(Film1Button,Film2Button,Film3Button,Film4Button,Film5Button,Film1,Film2,Film3,Film4,Film5,Date);
 
-//        window.setOnCloseRequest(e>) {
-//            e.consume;
-//            closeProgram()  );
+
+
+
+
+
+
+
 
 
 
         //Window 2
+
+
 
         GridPane grid2 = new GridPane();
         grid2.setPadding(new Insets(10,10,10,10)); //border
         grid2.setVgap(8);                                               //Spacing for elements
         grid2.setHgap(10);
 
-
-
-        //Button 2
-        Button button2 = new Button("Home");
-        button2.setOnAction(e -> window.setScene(HomeScreen));
-        GridPane.setConstraints(button2, 1,0);
-        grid2.getChildren().addAll(button2);
-
-
-
         FilmTimes = new Scene(grid2,visualBounds.getWidth(),visualBounds.getHeight());
+
+
+        Button HomeButton = new Button("Home");
+        HomeButton.setPrefHeight(25);
+        HomeButton.setPrefWidth(100);
+
+        Button[] FilmTimes;
+
+        FilmTimes = new Button[11];
+        for (int i = 1; i <11; i++){
+            FilmTimes[i] = new Button();
+            FilmTimes[i].setPrefHeight(100);
+            FilmTimes[i].setPrefWidth(200);
+            grid2.getChildren().add(FilmTimes[i]);
+
+        }
+
+        GridPane.setConstraints(FilmTimes[1], 50,10);
+        GridPane.setConstraints(FilmTimes[2], 50,20);
+        GridPane.setConstraints(FilmTimes[3], 50,30);
+        GridPane.setConstraints(FilmTimes[4], 50,40);
+        GridPane.setConstraints(FilmTimes[5], 50,50);
+        GridPane.setConstraints(FilmTimes[6], 60,10);
+        GridPane.setConstraints(FilmTimes[7], 60,20);
+        GridPane.setConstraints(FilmTimes[8], 60,30);
+        GridPane.setConstraints(FilmTimes[9], 60,40);
+        GridPane.setConstraints(FilmTimes[10], 60,50);
+
+        FilmTimes[1].setText("9:00");
+        FilmTimes[2].setText("11:00");
+        FilmTimes[3].setText("13:00");
+        FilmTimes[4].setText("15:00");
+        FilmTimes[5].setText("17:00");
+        FilmTimes[6].setText("18:00");
+        FilmTimes[7].setText("19:00");
+        FilmTimes[8].setText("20:00");
+        FilmTimes[9].setText("21:00");
+        FilmTimes[10].setText("22:00");
+
+
+
+
+
+
+
+        HomeButton.setOnAction(e -> {
+            window.setScene(HomeScreen);
+            TimeFor.setText("");
+        });
+
+
+
+
+        GridPane.setConstraints(HomeButton, 1,0);
+        GridPane.setConstraints(TimeFor, 50, 1);
+
+
+        grid2.getChildren().addAll(HomeButton,TimeFor);
+
+
+
+
 
         //Display scene 1 at first
         window.setScene(HomeScreen);
@@ -130,7 +217,12 @@ public class Home extends Application {
 
 
 
+
+
+
     }
+
+
 
     public void closeProgram(){ //Implement at a later date, stops user accidently closing program
         //boolean result = ConfirmBox.display("Exit", "Are you sure you want to exit");
@@ -138,6 +230,9 @@ public class Home extends Application {
 
 
     }
+
+
+
 
 
 
