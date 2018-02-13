5f600febc582286847860b0dcaa65872cdbaa553
Benjamin Ashby <sc16bira@leeds.ac.uk>
2018-02-13 00:16:38 +0000

    Updated forms.py

--- website/app/forms.py
+++ website/app/forms.py
@@ -1,20 +1,20 @@
 from flask_wtf import Form
 from wtforms import TextAreaField, StringField, TextField
-from wtforms.validators import DataRequired             #imports needed for form
+from wtforms.validators import DataRequired #Imports needed for form
 # from .models import Task
 
 class CreateForm(Form):
-    number2 = TextAreaField('number2', validators=[DataRequired()]) #the description field
+    number2 = TextAreaField('number2', validators=[DataRequired()]) #The description field
 
 class SessionForm(Form):
-    value = TextField('value', validators=[DataRequired()])             #used for login
+    value = TextField('value', validators=[DataRequired()]) #Used for login
     pw = TextField('pw', validators=[DataRequired()])
 
 class SignupForm(Form):
-    username = TextField('username', validators=[DataRequired()])      #used for signing up
+    username = TextField('username', validators=[DataRequired()]) #Used for signing up
     password = TextField('password', validators=[DataRequired()])
 
 class PasswordForm(Form):
     changeusername = TextField('changeusername', validators=[DataRequired()])
-    changepassword = TextField('changepassword', validators=[DataRequired()])   #used for changing password
+    changepassword = TextField('changepassword', validators=[DataRequired()]) #Used for changing password
     newpassword = TextField('newpassword', validators=[DataRequired()])
