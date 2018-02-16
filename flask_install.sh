9a83a18ae9cec00e024483fd8f49a0a5e13263ef
Taranvir Bola [sc16tb] <sc16tb@leeds.ac.uk>
2018-02-16 12:02:08 +0000

    Finished the flask installation shell script.

---
+++ flask_install.sh
@@ -0,0 +1,20 @@
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd )"
+echo $DIR
+module add python/3.4.3
+cd $DIR/website
+virtualenv $DIR/flask
+source $DIR/flask/bin/activate
+
+flask/bin/pip install flask
+flask/bin/pip install flask-login
+flask/bin/pip install flask-mail
+flask/bin/pip install flask-sqlalchemy
+flask/bin/pip install sqlalchemy-migrate
+flask/bin/pip install flask-whooshalchemy
+flask/bin/pip install flask-wtf
+flask/bin/pip install flask-babel
+flask/bin/pip install coverage
+flask/bin/pip install flask-admin
+
+from flask import Flask
+python run.py
