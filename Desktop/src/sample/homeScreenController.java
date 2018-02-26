7a62449ca8a7e1b71f29b5b41a6537146ce81bf3
Daniel Aves <sc16da@leeds.ac.uk>
2018-02-26 11:45:45 +0000

    Designed and added new homescreen ready for integration

---
+++ Desktop/src/sample/homeScreenController.java
@@ -0,0 +1,21 @@
+package sample;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+public class homeScreenController {
+
+    public void loginButtonClicked(ActionEvent event) throws IOException {
+
+
+    }
+
+
+}
