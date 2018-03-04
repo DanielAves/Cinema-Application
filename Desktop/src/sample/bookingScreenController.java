781998e2aa75e7297b5da98136d06a30dfd29afa
Daniel Aves <sc16da@leeds.ac.uk>
2018-03-04 23:09:34 +0000

    Fixed corrupt files causing issues in the interface

--- Desktop/src/sample/bookingScreenController.java
+++ Desktop/src/sample/bookingScreenController.java
@@ -1,7 +1,16 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+import java.io.IOException;
 
 public class bookingScreenController {
 
@@ -15,4 +24,32 @@ public class bookingScreenController {
 
 
     }
+
+
+    public void backButtonClicked(ActionEvent event) throws IOException {
+        Parent secondaryroot = FXMLLoader.load(getClass().getResource("resources/filmScreen.fxml"));
+        Scene filmScreen = new Scene(secondaryroot);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(filmScreen);
+        window.show();
+
+    }
+
+    public void logoutButtonClicked(ActionEvent event) throws IOException{
+        Parent secondaryroot = FXMLLoader.load(getClass().getResource("resources/loginScreen.fxml"));
+        Scene filmScreen = new Scene(secondaryroot);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(filmScreen);
+        window.show();
+
+    }
+
+    public void selectSeats(ActionEvent event) throws IOException{
+        Parent secondaryroot = FXMLLoader.load(getClass().getResource("resources/seatingScreen.fxml"));
+        Scene filmScreen = new Scene(secondaryroot);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(filmScreen);
+        window.show();
+    }
+
 }
