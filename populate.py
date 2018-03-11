a67147901c9db8333ba6af86fac978319bb548e4
Benjamin Ashby <sc16bira@leeds.ac.uk>
2018-03-11 15:44:15 +0000

    Altered populate.py to include ticket prices and film posters during population. Note that date population is still using strings as opposed to actual date type.

--- populate.py
+++ populate.py
@@ -22,18 +22,18 @@ c.executemany("INSERT INTO Card(card_id, card_number, card_expiry, card_cvv, cus
 (6, u'0234757982245075', u'11/23', u'654', 4)
 ])
 
-c.executemany("INSERT INTO Film(film_id, film_name, film_description, film_runtime, film_director, film_age_rating) VALUES (?, ?, ?, ?, ?, ?)",
+c.executemany("INSERT INTO Film(film_id, film_name, film_description, film_runtime, film_director, film_age_rating, film_poster) VALUES (?, ?, ?, ?, ?, ?, ?)",
 [
-(1, u'The Greatest Showman', u'American musical drama film, written by Jenny Bicks and starring Hugh Jackman, Zac Efront, Michelle Williams, Rebecca Ferguson and Zendaya', 120, u'Michael Gracey', u'12'),
-(2, u'Black Panther', u'With the King of Wakanda now dead, his son T’Challa must return home to take his rightful place on the throne. But, it’s going to be a bumpy ride, as an evil faction has other plans for the country’s future.', 134, u'Ryan Coogler', u'12'),
-(3, u'Coco', u'Ignoring his family’s baffling ban on music, Miguel embarks on a journey through the enchanting Land of the Dead in search of his dreams of becoming a musician.', 105, u'Adrian Molina', u'U'),
-(4, u'Downsizing', u'A married couple decide to escape the daily humdrum of modern life by downsizing – a procedure which will see their bodies shrink to 5 inches tall, but their modest savings turn into millions of pounds.', 135, u'Alexander Payne', u'15'),
-(5, u'Early Man', u'As the Bronze Age arrives and the Stone Age is over, a determined caveman and his faithful sidekick must fight for his home and the rights of his people. ', 89, u'Nick Park', u'U'),
-(6, u'Jumanji: Welcome To The Jungle', u'When four kids dust off an old video game and begin to play ‘Jumanji’, they quickly realise this is not a normal game. It’s a game of survival, where only the winners return to the real world.', 119, u'Jake Kasdan', u'12'),
-(7, u'Darkest Hour', u'The fate of Britain lies on Winston Churchill’s shoulders, in this historical-drama that tells the story of the country’s darkest – and strongest – hours.', 125, u'Joe Wright', u'U'),
-(8, u'The Post', u'In June 1971 The New York Times, the Washington Post and the nations major newspapers took a brave stand for freedom of speech and reported on the Pentagon Papers, the massive cover-up of government secrets that spanned four decades and four Presidents.', 116, u'Steven Spielberg', u'12'),
-(9, u'Maze Runner: The Death Cure', u'The final battle: Thomas must try to find a cure for the disease that has destroyed most of society. Live or die: everything is about to change...', 142, u'Wes Ball', u'12'),
-(10, u'Journeys End', u'With Operation Michael in full swing and a battle on the horizon, a group of British soldiers holed up in a dugout in Aisne in 1918 anxiously await their fate.', 107, u'Saul Dibb', u'12')
+(1, u'The Greatest Showman', u'American musical drama film, written by Jenny Bicks and starring Hugh Jackman, Zac Efront, Michelle Williams, Rebecca Ferguson and Zendaya', 120, u'Michael Gracey', u'12', ''),
+(2, u'Black Panther', u'With the King of Wakanda now dead, his son T’Challa must return home to take his rightful place on the throne. But, it’s going to be a bumpy ride, as an evil faction has other plans for the country’s future.', 134, u'Ryan Coogler', u'12', ''),
+(3, u'Coco', u'Ignoring his family’s baffling ban on music, Miguel embarks on a journey through the enchanting Land of the Dead in search of his dreams of becoming a musician.', 105, u'Adrian Molina', u'U', ''),
+(4, u'Downsizing', u'A married couple decide to escape the daily humdrum of modern life by downsizing – a procedure which will see their bodies shrink to 5 inches tall, but their modest savings turn into millions of pounds.', 135, u'Alexander Payne', u'15', ''),
+(5, u'Early Man', u'As the Bronze Age arrives and the Stone Age is over, a determined caveman and his faithful sidekick must fight for his home and the rights of his people. ', 89, u'Nick Park', u'U', ''),
+(6, u'Jumanji: Welcome To The Jungle', u'When four kids dust off an old video game and begin to play ‘Jumanji’, they quickly realise this is not a normal game. It’s a game of survival, where only the winners return to the real world.', 119, u'Jake Kasdan', u'12', ''),
+(7, u'Darkest Hour', u'The fate of Britain lies on Winston Churchill’s shoulders, in this historical-drama that tells the story of the country’s darkest – and strongest – hours.', 125, u'Joe Wright', u'U', ''),
+(8, u'The Post', u'In June 1971 The New York Times, the Washington Post and the nations major newspapers took a brave stand for freedom of speech and reported on the Pentagon Papers, the massive cover-up of government secrets that spanned four decades and four Presidents.', 116, u'Steven Spielberg', u'12', ''),
+(9, u'Maze Runner: The Death Cure', u'The final battle: Thomas must try to find a cure for the disease that has destroyed most of society. Live or die: everything is about to change...', 142, u'Wes Ball', u'12', ''),
+(10, u'Journeys End', u'With Operation Michael in full swing and a battle on the horizon, a group of British soldiers holed up in a dugout in Aisne in 1918 anxiously await their fate.', 107, u'Saul Dibb', u'12', '')
 ])
 
 c.executemany("INSERT INTO Screen(screen_id, screen_capacity) VALUES (?, ?)",
@@ -62,11 +62,11 @@ c.executemany("INSERT INTO Seat(seat_id) VALUES (?)",
 ('1'), ('2'), ('3'), ('4'), ('5'), ('6'), ('7'), ('8'), ('9')
 ])
 
-c.executemany("INSERT INTO Ticket(ticket_id, screening_id, customer_id, seat_id) VALUES (?, ?, ?, ?)",
+c.executemany("INSERT INTO Ticket(ticket_id, ticket_price, screening_id, customer_id, seat_id) VALUES (?, ?, ?, ?, ?)",
 [
-(1, 1, 1, 10), (2, 1, 1, 11), (3, 1, 1, 12), (4, 1, 2, 5), (5, 1, 2, 4),
-(6, 2, 3, 15), (7, 2, 1, 10), (8, 2, 1, 11), (9, 3, 4, 10), (10, 3, 4, 11),
-(11, 4, 1, 10), (12, 4, 1, 9), (13, 4, 4, 20), (14, 4, 4, 21), (15, 4, 4, 22)
+(1, u'£6.95', 1, 1, 10), (2, u'£6.95', 1, 1, 11), (3, u'£6.95', 1, 1, 12), (4, u'£6.45', 1, 2, 5), (5, u'£6.45', 1, 2, 4),
+(6, u'£9.95', 2, 3, 15), (7, u'£9.95', 2, 1, 10), (8, u'£16.95', 2, 1, 11), (9, u'£16.95', 3, 4, 10), (10, u'£6.95', 3, 4, 11),
+(11, u'£7.99', 4, 1, 10), (12, u'£7.99', 4, 1, 9), (13, u'£4.39', 4, 4, 20), (14, u'£6.29', 4, 4, 21), (15, u'£7.95', 4, 4, 22)
 ])
 
 db.commit()
