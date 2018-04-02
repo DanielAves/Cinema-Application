75042fc9af6b1399108e80d89712aacbeb3ad39b
Daniel Aves <sc16da@leeds.ac.uk>
2018-04-02 23:05:45 +0100

    Displays all filmnames in database sucessfully.

--- Desktop/src/RestClient/Harness.java
+++ Desktop/src/RestClient/Harness.java
@@ -1,5 +1,6 @@
 
 import java.util.List;
+import java.util.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.LocalDate ;
 
@@ -9,15 +10,43 @@ public class Harness {
       RestClient client = new RestClient("localhost", 5000);
       LocalDate d = LocalDate.of(1997 , 12, 20);
       Customer c = new Customer(5 , "Mitch" , "Gladstone" , d, "0007999977", "archery rd" , "Ls2 9au");
-      Seat seat = client.getSeat(2);
-      Screening s = client.getScreening(5);
+      // Seat seat = client.getSeat(2);
+      // Screening s = client.getScreening(5);
+      // Screening s = client.getScreening(5);
+       // Film f = client.getFilm(1);
+       // String filmName = f.getFilm_name();
+       // System.out.println(filmName);
 
-      System.out.println(s);
 
-      Ticket t = client.createTicket(c , s , seat );
 
-      ObjectMapper mapper = new ObjectMapper();
 
-      System.out.println(mapper.writeValueAsString(t));
+      for(int i =1; i<=10 ;i++){ //client.getFilm(i) != null
+        Film f = client.getFilm(i);
+        String filmName = f.getFilm_name();
+        System.out.println(filmName);
+      }
+
+
+
+
+      //String filmName = f.getFilm_name();
+      // for(Field field : client.getFilm().getDeclaredFields()) {
+      //   field.setAccessible(true);
+      //   String name = field.getFilm_name();
+      //   System.out.println(name);
+      // }
+
+
+      // for(f.getFilm_name){
+      //
+      // }
+
+
+
+      // Ticket t = client.createTicket(c , s , seat );
+      //
+      // ObjectMapper mapper = new ObjectMapper();
+      //
+      // System.out.println(mapper.writeValueAsString(t));
   }
 }
