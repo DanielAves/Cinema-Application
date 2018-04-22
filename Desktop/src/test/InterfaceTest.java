ae83d96c1b64168df4b88dcdc5b1cad76edd3962
Dan Aves <sc16da@leeds.ac.uk>
2018-04-22 16:54:33 +0100

    All seats are now bookable, some taken seats show as a different colour.

---
+++ Desktop/src/test/InterfaceTest.java
@@ -0,0 +1,24 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+
+
+/**
+ * Unit tests for the Desktop interface
+ */
+
+
+ public Class interfaceTest{
+
+
+   @Before
+
+
+
+
+
+
+
+ }
